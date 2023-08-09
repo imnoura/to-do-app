e98ff468e1222f2a8e091b7998a19fdf9d361d49
imnoura <imnouramousavi@gmail.com>
2023-08-09 19:53:55 +0330

    chore: bsae of the header button part

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button>To Do</button>
+    <button>Done</button>
+  </div>
+</template>
+
+<script setup>
+defineProps({});
+</script>
+
+<style scoped lang="postcss">
+button {
+  @apply bg-[#A880E4] rounded-sm border-2 border-solid border-[#A880E4] mb-5 py-7 px-36;
+}
+</style>
