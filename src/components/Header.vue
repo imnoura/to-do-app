2be184c2a2edf4da19e095275dfe600887e237e2
imnoura <imnouramousavi@gmail.com>
2023-08-13 00:37:04 +0330

    chore: add responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="@apply bg-[#8083e4] mb-5 rounded-lg">
+  <div
+    class="@apply flex w-[302px] h-16 sm:w-[623px] bg-[#8083e4] mb-5 rounded-lg"
+  >
     <button>To Do</button>
     <button>Done</button>
   </div>
@@ -11,7 +13,7 @@ defineProps({});
 
 <style scoped lang="postcss">
 button {
-  @apply bg-[#8083e4] rounded-lg py-7 px-32 text-2xl text-white;
+  @apply flex-1 justify-evenly bg-[#8083e4] rounded-lg text-2xl text-white;
 }
 button:hover {
   @apply bg-[#6b6fe6];
