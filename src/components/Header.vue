df6e1f2d32b47d87cba6803b020b811fe8ca1e98
imnoura <imnouramousavi@gmail.com>
2023-08-12 18:24:41 +0330

    chore: add the header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="@apply bg-[#8083e4] mb-5 rounded-lg">
     <button>To Do</button>
     <button>Done</button>
   </div>
@@ -11,6 +11,9 @@ defineProps({});
 
 <style scoped lang="postcss">
 button {
-  @apply bg-[#A880E4] rounded-sm border-2 border-solid border-[#A880E4] mb-5 py-7 px-36;
+  @apply bg-[#8083e4] rounded-lg py-7 px-32 text-2xl text-white;
+}
+button:hover {
+  @apply bg-[#6b6fe6];
 }
 </style>
