1b6f8bee60748ecfab9bc3b9019e2f74da4ef7f4
imnoura <imnouramousavi@gmail.com>
2023-08-13 22:04:06 +0330

    chore: change component's name in the codes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="overall">
     <Header />
-    <TaskColumn />
+    <TaskList />
   </div>
 </template>
 
 <script setup>
 import Header from "./components/Header.vue";
-import TaskColumn from "./components/TaskColumn.vue";
+import TaskList from "./components/TaskList.vue";
 </script>
 
 <style scoped>
