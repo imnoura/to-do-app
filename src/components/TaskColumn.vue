90ccd11ec0311130ce02b8f71af9a9e335707b44
imnoura <imnouramousavi@gmail.com>
2023-08-13 14:22:30 +0330

    style: inhance ui

--- src/components/TaskColumn.vue
+++ src/components/TaskColumn.vue
@@ -4,26 +4,26 @@
       <input type="text" placeholder="Add new..." />
       <button>Add</button>
     </form>
-
-    <hr />
-
     <div class="todo-list">
-      <div class="todo completed">
+      <div class="todo">
         <div class="todo__info">
-          <input type="checkbox" checked />
-          <p>Buy a new laptop!</p>
+          <p class="break-all sm:break-auto">
+            Buy a new
+            laptop!xcvhjkjhgfchgfdfghsdfsdfghsdfgnhfthsdfgfdghsdgfsdfgsdf
+          </p>
         </div>
         <div class="todo__actions">
-          <button>❌</button>
+          <button>☑</button>
+          <button>☒</button>
         </div>
       </div>
       <div class="todo">
         <div class="todo__info">
-          <input type="checkbox" />
           <p>Buy some milk</p>
         </div>
         <div class="todo__actions">
-          <button>❌</button>
+          <button>☑</button>
+          <button>☒</button>
         </div>
       </div>
     </div>
@@ -33,23 +33,16 @@
 <script setup></script>
 
 <style scoped>
-h1 {
-  color: rgb(69, 69, 69);
-  font-weight: 900;
-  margin-bottom: 48px;
-  text-align: center;
-}
-
 form {
   @apply sm:w-[595px] justify-evenly flex sm:flex-row flex-col mb-6 mx-4;
 }
 
 form input {
-  @apply sm:w-[400px] text-2xl px-3 sm:py-4 py-3 bg-[#ccceff] border-solid border-2 border-[#bbbdf6] rounded-lg;
+  @apply sm:w-[400px] text-[#4101cc] text-2xl px-3 sm:py-4 py-3 bg-[#ccceff] border-solid border-2 border-[#bbbdf6] rounded-lg;
 }
 
 form button {
-  @apply sm:mt-0 mt-3 text-2xl px-16 sm:py-4 py-3 rounded-lg border-solid border-2 bg-[#8083e4] text-white;
+  @apply sm:mt-0 mt-3 text-2xl px-16 sm:py-4 py-3 rounded-lg bg-[#8083e4] text-white;
 }
 
 form button:hover {
@@ -62,49 +55,35 @@ form button[disabled] {
   cursor: not-allowed;
   color: rgb(230, 230, 230);
 }
-hr {
-  margin-bottom: 18px;
-  display: block;
-  opacity: 0.3;
-}
 
 .todo-box {
-  @apply bg-[#dedfff] rounded-lg py-7 mx-7 shadow-lg;
+  @apply bg-[#dedfff] rounded-lg py-7 mx-7 shadow-lg w-[302px] sm:w-[630px];
 }
 
 .todo-list {
-  @apply flex flex-col gap-4 mx-5;
+  @apply flex flex-col gap-4 mt-8 mx-4 sm:mx-5;
 }
 
 .todo {
-  @apply flex justify-between items-center bg-[#8083e4] rounded px-3 sm:py-4 py-3  w-auto;
+  @apply grid grid-cols-1 sm:grid-cols-12 sm:gap-12 gap-2 text-center sm:text-left justify-between items-center bg-[#8083e4] rounded pl-2 sm:py-4 py-3;
 }
 
 .todo:hover {
   @apply bg-[#6b6fe6];
 }
 
-.todo.completed {
-  @apply opacity-40;
-}
-
-.todo.completed p {
-  @apply line-through;
-}
-
 .todo .todo__info {
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  font-size: 24px;
+  @apply sm:col-span-9 gap-2 sm:text-2xl text-xl text-white;
 }
 
 .todo__actions button {
-  @apply text-2xl border-none cursor-pointer opacity-70 duration-200 bg-transparent;
+  @apply sm:text-4xl text-2xl text-white cursor-pointer sm:ml-3 ml-2  opacity-80 duration-200;
 }
-
+.todo__actions {
+  @apply col-span-3;
+}
 .todo__actions button:hover {
-  opacity: 1;
+  @apply opacity-100;
 }
 
 .todo input {
